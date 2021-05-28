e5277508394c8793201780a99bc102724694dd26
boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2021-05-29 04:22:51 +0530

    scraping multiple pages at same time if we have link in one page

---
+++ Scraping one page using scrapy/Readme.md
@@ -0,0 +1,14 @@
+# For exporting the scraped data 
+
+```
+scrapy crawl countries -o data.csv
+```
+
+```
+scrapy crawl countries -o data.json
+```
+
+```
+scrapy crawl countries -o data.xml
+```
+
--- Scraping one page using scrapy/worldometerspopulation/worldometerspopulation/spiders/countries.py
+++ Scraping one page using scrapy/worldometerspopulation/worldometerspopulation/spiders/countries.py
@@ -1,9 +1,9 @@
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -12,8 +12,22 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
+            #absolute_url =f"https://www.worldometers.info/{link}" #Way 1
+            #absolute_url =  response.urljoin(link)  #Way 2
+            # yield scrapy.Request(url=absolute_url)
+            yield response.follow(url=link, callback=self.parse_country, meta={"country_name":name})
 
-            yield {
-                'country_name': name,
-                'country_link': link
+# Meta help to sync the extracted data between more than one parsing methods 
+    def parse_country(self, response):
+        country_name = response.request.meta["country_name"]
+        rows = response.xpath('//div[@class="table-responsive"]/table[@class="table table-striped table-bordered table-hover table-condensed table-list"][1]/tbody/tr')
+        for r in rows:
+            year = r.xpath(".//td[1]/text()").get()
+            population = r.xpath(".//td[2]/strong/text()").get()
+
+            yield{
+                "country_name": country_name,
+                "year":year,
+                "population": population
             }
+
