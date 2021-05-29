fe97886cd72ee6e31714c5bde2549d1ca1998e9b
boltuzamaki <divyanshuboltuzamaki23@gmail.com>
2021-05-29 17:53:00 +0530

    spoofing request header

--- Scraping pages with pagination and spoofing agent/glasseshop/glasseshop/settings.py
+++ Scraping pages with pagination and spoofing agent/glasseshop/glasseshop/settings.py
@@ -38,8 +38,7 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+#    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 #}
 
 # Enable or disable spider middlewares
--- Scraping pages with pagination and spoofing agent/glasseshop/glasseshop/spiders/glasses.py
+++ Scraping pages with pagination and spoofing agent/glasseshop/glasseshop/spiders/glasses.py
@@ -4,7 +4,12 @@ import re
 class GlassesSpider(scrapy.Spider):
     name = 'glasses'
     allowed_domains = ['www.glassesshop.com']
-    start_urls = ['https://www.glassesshop.com/bestsellers']
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.glassesshop.com/bestsellers", callback=self.parse,
+        headers={
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+        })
 
     def parse(self, response):
         for glass in response.xpath('//div[@id="product-lists"]/div[@class="col-12 pb-5 mb-lg-3 col-lg-4 product-list-row text-center product-list-item"]'):
@@ -20,9 +25,12 @@ class GlassesSpider(scrapy.Spider):
                 "glass_link": glass_link,
                 "glass_image_link": glass_image_link,
                 "glass_name": glass_name,
-                "glass_price": glass_price
+                "glass_price": glass_price,
+                "User-Agent": response.request.headers['User-Agent']
             }
         
         next_page = response.xpath('//div[@class="row mt-5 mb-5 d-none d-lg-block"]/div/ul/li[6][@class="page-item"]/a/@href').get()
         if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+            })
